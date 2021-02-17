35b421b9a9a23e98fd939892be457d2770b1d340
Ramez <ramezmmbell@gmail.com>
2021-02-17 23:05:52 +0200

    hiiiiii deeeb

--- lib/Home_Page.dart
+++ lib/Home_Page.dart
@@ -14,17 +14,15 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:praytimes/helper/local_notifications_helper.dart';
 //import 'package:easy_localization/easy_localization.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 NotificationAppLaunchDetails notificationAppLaunchDetails;
 
-
-
 void main() async {
-
   runApp(Home());
-  notificationAppLaunchDetails = await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
   await initNotifications(flutterLocalNotificationsPlugin);
-
 }
 
 /*
@@ -47,24 +45,24 @@ class StartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hi',
+      title: 'Hii deeebb',
       home: Playground(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
 class Home extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
         home: Scaffold(
-          body:
-          HomePage(),
-        ));
+      body: HomePage(),
+    ));
   }
 }
+
 String locale = 'ar';
 
 class Clock extends StatefulWidget {
@@ -74,23 +72,24 @@ class Clock extends StatefulWidget {
 }
 
 class _Clock extends State<Clock> {
-  double minutesAngle=0;
-  double secondsAngle=0;
-  double hoursAngle=0;
+  double minutesAngle = 0;
+  double secondsAngle = 0;
+  double hoursAngle = 0;
   Timer timer;
 
   @override
   void initState() {
     super.initState();
     timer = Timer.periodic(Duration(milliseconds: 500), (timer) {
-      final now= DateTime.now();
+      final now = DateTime.now();
       setState(() {
-        secondsAngle= (pi/30)*now.second;
-        minutesAngle= (pi/30)* now.minute;
-        hoursAngle=(pi/6 *now.hour)+(pi/45 *minutesAngle);
+        secondsAngle = (pi / 30) * now.second;
+        minutesAngle = (pi / 30) * now.minute;
+        hoursAngle = (pi / 6 * now.hour) + (pi / 45 * minutesAngle);
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -109,7 +108,7 @@ class _Clock extends State<Clock> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                alignment: Alignment(0,-0.35),
+                alignment: Alignment(0, -0.35),
               ),
               angle: secondsAngle,
             ),
@@ -124,7 +123,7 @@ class _Clock extends State<Clock> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                alignment: Alignment(0,-0.35),
+                alignment: Alignment(0, -0.35),
               ),
               angle: minutesAngle,
             ),
@@ -139,7 +138,7 @@ class _Clock extends State<Clock> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                alignment: Alignment(0,-0.25),
+                alignment: Alignment(0, -0.25),
               ),
               angle: hoursAngle,
             ),
@@ -149,7 +148,6 @@ class _Clock extends State<Clock> {
         width: 270,
         height: 270,
       ),
-
       alignment: Alignment.center,
     );
   }
@@ -184,10 +182,10 @@ Widget imageSection = Column(
 );
 
 Widget prayTime = Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      PrayTimes(),
-    ],
+  mainAxisSize: MainAxisSize.min,
+  children: [
+    PrayTimes(),
+  ],
 );
 
 Widget buttonSection = Container(
@@ -230,7 +228,6 @@ class PrayTimes extends StatefulWidget {
 }
 
 class _PrayTimesState extends State<PrayTimes> {
-
   JsonConnection jsonConnection = new JsonConnection();
   Data list;
   static double pLat;
@@ -245,8 +242,8 @@ class _PrayTimesState extends State<PrayTimes> {
   //http://api.aladhan.com/v1/timingsByCity?city=$city&country=$country&method=$method
 */
   Future getPTLocation() async {
-
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
 
     pLat = position.latitude;
     pLong = position.longitude;
@@ -254,11 +251,12 @@ class _PrayTimesState extends State<PrayTimes> {
     String date = DateTime.now().toIso8601String();
     int method = 5;
 
-    final url = "http://api.aladhan.com/v1/timings/$date?latitude=$pLat&longitude=$pLong&method=$method";
+    final url =
+        "http://api.aladhan.com/v1/timings/$date?latitude=$pLat&longitude=$pLong&method=$method";
 
     http.Response res = await http.get(Uri.encodeFull(url), headers: {
       "Accept":
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
     });
 
     final data = json.decode(res.body);
@@ -284,11 +282,17 @@ class _PrayTimesState extends State<PrayTimes> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
-                          Text('Fajr',style: TextStyle(fontSize: 30),),
-                          Text('Dhuhr',style: TextStyle(fontSize: 30),),
-                          Text('Asr',style: TextStyle(fontSize: 30)),
-                          Text('Maghrib',style: TextStyle(fontSize: 30)),
-                          Text('Isha',style: TextStyle(fontSize: 30)),
+                          Text(
+                            'Fajr',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          Text(
+                            'Dhuhr',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          Text('Asr', style: TextStyle(fontSize: 30)),
+                          Text('Maghrib', style: TextStyle(fontSize: 30)),
+                          Text('Isha', style: TextStyle(fontSize: 30)),
                         ],
                       ),
                     ),
@@ -296,11 +300,16 @@ class _PrayTimesState extends State<PrayTimes> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
-                          Text(snapshot.data.data.timings.fajr,style: TextStyle(fontSize: 30)),
-                          Text(snapshot.data.data.timings.dhuhr,style: TextStyle(fontSize: 30)),
-                          Text(snapshot.data.data.timings.asr,style: TextStyle(fontSize: 30)),
-                          Text(snapshot.data.data.timings.maghrib,style: TextStyle(fontSize: 30)),
-                          Text(snapshot.data.data.timings.isha,style: TextStyle(fontSize: 30)),
+                          Text(snapshot.data.data.timings.fajr,
+                              style: TextStyle(fontSize: 30)),
+                          Text(snapshot.data.data.timings.dhuhr,
+                              style: TextStyle(fontSize: 30)),
+                          Text(snapshot.data.data.timings.asr,
+                              style: TextStyle(fontSize: 30)),
+                          Text(snapshot.data.data.timings.maghrib,
+                              style: TextStyle(fontSize: 30)),
+                          Text(snapshot.data.data.timings.isha,
+                              style: TextStyle(fontSize: 30)),
                         ],
                       ),
                     ),
@@ -308,7 +317,7 @@ class _PrayTimesState extends State<PrayTimes> {
                 ),
               ),
             );
-          } else  {
+          } else {
             return CircularProgressIndicator();
           }
         },
@@ -316,5 +325,3 @@ class _PrayTimesState extends State<PrayTimes> {
     );
   }
 }
-
-
